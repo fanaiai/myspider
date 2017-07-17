f35863d929780591d39068720f993489f91c557a
fanaiai <516039800@qq.com>
2017-07-17 19:21:52 +0800

    Merge branch 'master' of https://github.com/fanaiai/myspider

--- myspider/pipelines.py
+++ myspider/pipelines.py
@@ -6,7 +6,7 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymysql.cursors
 import logging
-from myspider.items import JdlistItem,JdItem,TaobaolistItem,TaobaoItem
+from myspider.items import JdlistItem,JdItem,TaobaolistItem,TaobaoItem,YihaoItem
 
 class InsertMysql(object):
 	def open_spider(self,spider):
@@ -20,8 +20,10 @@ class InsertMysql(object):
 	def process_item(self,item,spider):
 		if isinstance(item,JdlistItem):
 			self.cursor.execute('insert into jdlist(name,url) values("%s","%s")' % (item['name'],item['url']))
-		if isinstance(item,TaobaolistItem):
+		elif isinstance(item,TaobaolistItem):
 			self.cursor.execute('insert into taobaolist(name,url) values("%s","%s")' % (item['name'],item['url']))
+		elif isinstance(item,YihaoItem):
+			self.cursor.execute('insert into yihaolist(name,url) values("%s","%s")' % (item['name'],item['url']))
 		elif isinstance(item,JdItem):
 			self.cursor.execute('insert into jiadian(id,name,price,brand,parameter,summary_service,url,sales_promotion,add_service,store_link,commentsnum,goodcomments,comment_tags,goodcommentnum,mediumcommentnum,badcommentnum,store,images_url) values("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")' % (item['id'],item['name'],item['price'],item['brand'],item['parameter'],item['summary_service'],item['url'],item['sales_promotion'],item['add_service'],item['store_link'],item['commentsnum'],item['goodcomments'],item['comment_tags'],item['goodcommentnum'],item['mediumcommentnum'],item['badcommentnum'],item['store'],item['images_url']))
 		elif isinstance(item,TaobaoItem):
